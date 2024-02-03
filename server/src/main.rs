43a5b3cebbf0f47fdf4afaf27fc81e748e8821e1
Zain Azam <zainyusufazam@gmail.com>
2024-02-03 13:41:51 -0500

    server stuff ez ez

---
+++ server/src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
