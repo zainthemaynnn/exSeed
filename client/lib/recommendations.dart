d409ba6c2d7c391a374d1e17cf58cb812d0a90f3
Rongbin99 <rongbingu999@gmail.com>
2024-02-03 21:14:20 -0500

    done recommendations fr

--- client/lib/recommendations.dart
+++ client/lib/recommendations.dart
@@ -21,6 +21,7 @@ class Recommendations extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Recommendations'),
